e39265034fa112665e147ae3452dba8c409dfbd3
wachirawahomed <wachirawahomed@gmail.com>
2024-04-20 02:03:12 +0300

    1-&-2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-# Using Puppet manifest to install Flask package
+# Puppet manifest to install Flask package
 
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
 }
 
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,9 +1,8 @@
 # Puppet manifest to kill a process named "killmenow"
 # Must use 'exec' and 'pkill'
 
-exec { 'killmenow':
-  command     => 'pkill -f killmenow',
-  path        => '/bin:/usr/bin',
-  refreshonly => true,
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
 
