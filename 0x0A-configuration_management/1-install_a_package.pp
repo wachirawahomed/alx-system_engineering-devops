574b95fa6e4299f4dc7b0fc959e7b1950faa691a
wachirawahomed <wachirawahomed@gmail.com>
2024-04-20 03:19:00 +0300

    1-update-5

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,18 @@
-# Using Puppet manifest to install Flask package
+# Puppet manifest to install Flask package and its dependencies
+
+package { 'python3':
+  ensure => '3.8.10',
+}
 
 package { 'flask':
-  ensure   =>  '2.1.0',
-  provider =>  'pip3',
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3'],
 }
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['python3'],
+}
+
