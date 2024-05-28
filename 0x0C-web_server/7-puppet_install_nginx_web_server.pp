104750974c29ef8ce270937bb3635d565d92466a
wachirawahomed <wachirawahomed@gmail.com>
2024-05-28 22:56:49 +0300

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,47 +1,24 @@
 # Puppet manifest to install and configure Nginx web server
 
-# Ensure Nginx package is installed
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
+
 package { 'nginx':
-  ensure => present,
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
-# Ensure Nginx service is running and enabled
-service { 'nginx':
-  ensure => running,
-  enable => true,
-  require => Package['nginx'],
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
 }
 
-# Configure Nginx to listen on port 80
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => template('nginx/default.erb'), # Assuming you have a template for Nginx configuration
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
-# Ensure custom 404 page is configured
-file { '/usr/share/nginx/html/404.html':
-  ensure  => file,
-  content => "Ceci n'est pas une page",
-}
-
-# Ensure Nginx returns 200 when querying /
-file { '/usr/share/nginx/html/index.html':
-  ensure  => file,
-  content => "Hello World!\n",
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Ensure Nginx returns a 301 redirect for /redirect_me
-nginx::resource::vhost { 'redirect_me':
-  www_root     => '/usr/share/nginx/html',
-  ensure       => present,
-  listen_port  => 80,
-  server_name  => 'localhost',
-  location_cfg => {
-    'return' => '301 https://www.youtube.com/watch?v=QH2-TGUlwu4;',
-  },
-  require => Package['nginx'],
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
 }
