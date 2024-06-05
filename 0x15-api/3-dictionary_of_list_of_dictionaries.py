e78500c4a1e1b32db938a459a96f691d867cb355
wachirawahomed <wachirawahomed@gmail.com>
2024-06-05 23:47:13 +0300

    3-dictionary_of_list_of_dictionaries.py updated

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,49 +1,36 @@
 #!/usr/bin/python3
 """
-Script to fetch tasks for all users and export them to JSON format.
+Fetches data from JSONPlaceholder API for all users'
+todo lists and exports to JSON
 """
 import json
 import requests
-import sys
+
+API = "https://jsonplaceholder.typicode.com"
 
 if __name__ == "__main__":
-    # Fetch tasks for all users
-    users_url = "https://jsonplaceholder.typicode.com/users"
-    tasks_url = "https://jsonplaceholder.typicode.com/todos"
+    users_response = requests.get(f"{API}/users")
+    todos_response = requests.get(f"{API}/todos")
 
-    try:
-        users_response = requests.get(users_url)
-        tasks_response = requests.get(tasks_url)
+    users = users_response.json()
+    todos = todos_response.json()
 
-        users_data = users_response.json()
-        tasks_data = tasks_response.json()
+    all_users = {}
 
-        # Organize tasks by user ID
-        tasks_by_user = {}
-        for task in tasks_data:
-            user_id = task["userId"]
-            task_entry = {
-                    "username": "",
-                    "task": task["title"],
-                    "completed": task["completed"]
-                    }
-            # Find username corresponding to user ID
-            for user in users_data:
-                if user["id"] == user_id:
-                    task_entry["username"] = user["username"]
-                    break
-            # Append task entry to user's list of tasks
-            if user_id in tasks_by_user:
-                tasks_by_user[user_id].append(task_entry)
-            else:
-                tasks_by_user[user_id] = [task_entry]
+    for user in users:
+        user_id = user.get("id")
+        username = user.get("username")
 
-        # Export data to JSON format
-        with open("todo_all_employees.json", "w") as json_file:
-            json.dump(tasks_by_user, json_file)
+        user_tasks = [
+            {
+                "username": username,
+                "task": todo.get("title"),
+                "completed": todo.get("completed"),
+            }
+            for todo in todos if todo.get("userId") == user_id
+        ]
 
-        print("Data exported to todo_all_employees.json")
+        all_users[user_id] = user_tasks
 
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        sys.exit(1)
+    with open("todo_all_employees.json", "w") as json_file:
+        json.dump(all_users, json_file)
