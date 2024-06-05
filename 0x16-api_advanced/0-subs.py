288c57d30574387c7765fd9bba45d9ecb141f827
wachirawahomed <wachirawahomed@gmail.com>
2024-06-05 21:05:36 +0300

    0-subs.py update 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -20,11 +20,10 @@ def number_of_subscribers(subreddit):
     """
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
         if 'data' in data and 'subscribers' in data['data']:
-            subs = data['data']['subscribers']
-            return subs
+            return data['data']['subscribers']
     return 0
