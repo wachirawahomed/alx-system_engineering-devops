2fa3071abdee804ba6161393b5de9824adb4c29f
wachirawahomed <wachirawahomed@gmail.com>
2024-06-12 22:51:29 +0300

    all tasks

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,24 @@
+# This Puppet manifest optimizes Nginx configuration for handling high concurrency.
+
+exec { 'fix--for-nginx':
+  command => '/usr/sbin/nginx -s reload',
+  refreshonly => true,
+}
+
+file { '/etc/nginx/nginx.conf':
+  ensure  => file,
+  content => template('nginx/nginx.conf.erb'),
+  notify  => Exec['fix--for-nginx'],
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => template('nginx/default.erb'),
+  notify  => Exec['fix--for-nginx'],
+}
+
+class { 'nginx':
+  package_ensure => 'present',
+  service_ensure => 'running',
+  service_enable => true,
+}
