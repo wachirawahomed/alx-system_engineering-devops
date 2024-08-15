a985a2a79c059caf3947199aaba2344d8f9ec99b
wachirawahomed <wachirawahomed@gmail.com>
2024-08-15 20:23:12 +0300

    0-subs.py update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,12 +3,13 @@
 A function that queries the Reddit API and returns the number of subscribers
 for a given subreddit. If an invalid subreddit is given, it returns 0.
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
